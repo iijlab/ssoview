49b49bfc24a976320ebdcec221868e9c94d5cb95
Kenzo Yotsuya <1560182+yotsuya@users.noreply.github.com>
2026-09-04 17:34:47 +0900

    Remove unused SessionSummary type guards

--- src/common/models/session-summary.ts
+++ src/common/models/session-summary.ts
@@ -4,7 +4,6 @@
  */
 
 import { createLabeledDebugLogger } from "@/common/utils/labeled-logger.ts";
-import { isObject } from "@/common/utils/type-guard.ts";
 
 export type SessionSummary = {
   protocol: SessionSsoProtocol;
@@ -20,39 +19,9 @@ export type SessionSummary = {
   warning: string[];
 };
 
-export function isSessionSummary(u: unknown): u is SessionSummary {
-  return (
-    isObject(u) &&
-    isSessionSsoProtocol(u.protocol) &&
-    typeof u.imported === "boolean" &&
-    typeof u.capturing === "boolean" &&
-    typeof u.sessionId === "string" &&
-    (!("start" in u) || typeof u.start === "string") &&
-    (!("end" in u) || typeof u.end === "string") &&
-    (!("sp" in u) || typeof u.sp === "string") &&
-    (!("idp" in u) || typeof u.idp === "string") &&
-    (!("status" in u) || isSessionStatus(u.status)) &&
-    (!("action" in u) || typeof u.action === "string") &&
-    Array.isArray(u.warning) &&
-    u.warning.every((w) => typeof w === "string")
-  );
-}
-
-export function isSessionSummaryArray(u: unknown): u is SessionSummary[] {
-  return Array.isArray(u) && u.every(isSessionSummary);
-}
-
 type SessionSsoProtocol = "saml" | "oidc";
 
-function isSessionSsoProtocol(u: unknown): u is SessionSsoProtocol {
-  return u === "saml" || u === "oidc";
-}
-
 type SessionStatus = "in_progress" | "succeeded" | "failed";
-
-function isSessionStatus(u: unknown): u is SessionStatus {
-  return u === "in_progress" || u === "succeeded" || u === "failed";
-}
 
 //
 // Debug utilities
